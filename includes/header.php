4424e12d7104166067d5e7020c5ff62ebb5a36f4
Jay Soorjun <jay.soorjun@gmail.com>
2018-04-03 21:55:34 +0400

    Initial Commit

---
+++ includes/header.php
@@ -0,0 +1,62 @@
+<!-- Header-->
+<nav class="uk-navbar">
+    <div class="uk-container uk-container-center">
+        <a href="" class="uk-navbar-brand uk-hidden-small">LOGO</a>
+        <ul class="uk-navbar-nav uk-hidden-small">
+            <li class="uk-active">
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">About</a>
+            </li>
+            <li>
+                <a href="">Services</a>
+            </li>
+            <li class="uk-parent">
+                <a href="">Contact</a>
+            </li>
+        </ul>
+        <div class="uk-navbar-flip uk-hidden-small">
+            <ul class="uk-navbar-nav">
+                <li>
+                    <a href="">
+                        <i class="uk-icon-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="uk-icon-facebook"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="uk-icon-youtube-play"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <a href="#offcanvas-nav" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas></a>
+        <div class="uk-navbar-content uk-navbar-center uk-visible-small">LOGO</div>
+
+    </div>
+</nav>
+<div id="offcanvas-nav" class="uk-offcanvas">
+    <div class="uk-offcanvas-bar">
+        <ul class="uk-nav uk-nav-offcanvas" data-uk-nav>
+            <li class="uk-active">
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">About</a>
+            </li>
+            <li>
+                <a href="">Services</a>
+            </li>
+            <li class="uk-parent">
+                <a href="">Contact</a>
+            </li>
+        </ul>
+    </div>
+</div>
+<!-- End Header-->
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>UIkit website</title>
+	<?php include'includes/head.php' ?>
+</head>
+
+<body>
+	<?php include'includes/header.php' ?>
+	<div class="uk-block uk-block-secondary">
+		asdasd
+	</div>
+
+	<!-- link to JQuery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<!-- link to uikit js -->
+	<script type="text/javascript" src="uikit/js/uikit.min.js"></script>
+</body>
+
+</html>
